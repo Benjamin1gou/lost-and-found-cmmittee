ef53728c5d3fb843f2b6eabbf70fc916f8a2bbdd
Benjamin1gou <yankuma4@gmail.com>
2020-01-22 23:56:48 +0900

    add aseart MongoDB接続テスト処理追加

--- backend/classes/mongo.js
+++ backend/classes/mongo.js
@@ -1,5 +1,27 @@
+/**
+ * MongoDB操作用クラス
+ * Created:2020.01.22 Benjamin1gou
+ * @constructor db 
+ */
 module.exports = class MongoClass{
     constructor (db){
         this.mongoClient = db
+        this.assert = require('assert')
     }
+
+    /**
+     * 接続用テスト
+     */
+    mongoConnect(){
+        this.mongoClient.connect('mongodb://127.0.0.1:27017/test', (err, db) =>{
+          this.assert.equal(null, err)
+          console.log('Connected success')
+          db.close()
+        })
+    }
+
+    /**
+     * 変更履歴登録
+     */
+    
 }
--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,11 @@ const mongo = require('mongodb')
 const mongoClient = mongo.MongoClient
 // corsポリシーに抵触するため、その対策としてcorsを利用する
 const cors = require('cors')
+// MongoClass呼び出し
 const mongoClass = require('./classes/mongo.js')
 let mongoItem = new mongoClass(mongoClient);
+// DB接続テスト
+mongoItem.mongoConnect()
 
 const app = express()
 app.use(bodyParser.json())
